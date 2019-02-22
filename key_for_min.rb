02a66e28e5c00890591cf337bf33846370415ac0
Mickae Truman <mtruman@nyit.edu>
2019-02-22 03:17:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -13,15 +13,15 @@ end
 smallest_value
 end
 #def key_for_min_value(item)
-  smallest_value = nil
-  other_value = nil
-  item.each do |key, value|
-    if other_value == nil || value < other_value
-      other_value = value
-      smallest_value = key
-    end
-  end
-  smallest_value
+  #smallest_value = nil
+  #other_value = nil
+  #item.each do |key, value|
+    #if other_value == nil || value < other_value
+      #other_value = value
+      #smallest_value = key
+    #end
+  #end
+  #smallest_value
 #end
 
 #end
