607a5eee0434117aff4a7045f596353f90128ef7
Mickae Truman <mtruman@nyit.edu>
2019-02-22 03:16:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,13 +1,13 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-#def key_for_min_value(item)
- # smallest_value = nil 
- # other_value = nil
- # item.each do |key, value|
-  #  if other_value == nil || value < other_value
-   #    other_value = value 
-    #   smallest_value = key
+def key_for_min_value(item)
+  smallest_value = nil 
+  other_value = nil
+  item.each do |key, value|
+    if other_value == nil || value < other_value
+      other_value = value 
+      smallest_value = key
 end
 end
 #smallest_value
@@ -22,7 +22,7 @@ end
     end
   end
   smallest_value
-end
+#end
 
 #end
 #def key_for_min_value(item)
